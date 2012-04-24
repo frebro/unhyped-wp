b73cfa30d2964e7263e5f13d9c15e3150c7e99b0
Fredrik Broman <frebro@gmail.com>
2012-04-24 13:39:27 +0200

    new header meta for device scaling

---
+++ header-meta.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Meta data in header.php
+ * Create your own header-meta.php to override these settings with your own
+ *
+ * @package WordPress
+ * @subpackage SYNACK
+ */
+?>
+
+<meta charset="<?php bloginfo( 'charset' ); ?>">
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+<meta name="viewport" content="width=device-width">
