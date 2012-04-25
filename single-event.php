f54205e52a38fabec161323942e30c77a6150696
Fredrik Broman <frebro@gmail.com>
2012-04-25 11:54:08 +0200

    sidebar widgets isolated into boxes

--- single-event.php
+++ single-event.php
@@ -23,32 +23,36 @@ get_header(); ?>
     </div><!-- #main -->
 
     <aside id="complementary" role="complementary">
-      <div class="meta">
-        <?php
-        // The date strings
-        if ( eo_get_the_start('Y-m-d') == date('Y-m-d') ):
-          $datestr = __('Today', 'unhyped');
-        elseif ( eo_get_the_start('Y-m-d') == date('Y-m-d', time()+86400) ):
-          $datestr = __('Tomorrow', 'unhyped');
-        else:
-          $datestr = eo_get_the_start('Y-m-d');
-        endif;
+      <div id="sidebar-eventmeta" class="sidebar">
+        <div class="widget-area">
+          <div class="widget">
+            <?php
+            // The date strings
+            if ( eo_get_the_start('Y-m-d') == date('Y-m-d') ):
+              $datestr = __('Today', 'unhyped');
+            elseif ( eo_get_the_start('Y-m-d') == date('Y-m-d', time()+86400) ):
+              $datestr = __('Tomorrow', 'unhyped');
+            else:
+              $datestr = eo_get_the_start('Y-m-d');
+            endif;
 
-        // The venue strings
-        $venue = eo_get_venue_name();
-        $address = eo_get_venue_address();
+            // The venue strings
+            $venue = eo_get_venue_name();
+            $address = eo_get_venue_address();
 
-        if (!empty($address['address']) && !empty($address['postcode'])) echo '<img class="gmap" src="http://maps.googleapis.com/maps/api/staticmap?maptype=roadmap&zoom=14&size=300x300&markers=color:0xFF0033|'.urlencode($address['address'].' '.$address['postcode']).'&sensor=false">';
+            if (!empty($address['address']) && !empty($address['postcode'])) echo '<a href="http://maps.google.com/maps?q='.urlencode($address['address'].' '.$address['postcode']).'"><img class="gmap" src="http://maps.googleapis.com/maps/api/staticmap?maptype=roadmap&zoom=14&size=600x600&markers=color:0xFF0033|'.urlencode($address['address'].' '.$address['postcode']).'&sensor=false"></a>';
 
-        // Event and venue meta
-        echo '<p class="entry-meta"><span class="date">'.$datestr.'</span>';
-        if (!empty($venue)) echo ' at <span class="location">'.$venue.'</span>';
-        if (!empty($address['address']) && !empty($address['postcode'])) echo '<br><a class="address" href="http://maps.google.com/maps?q='.urlencode($address['address'].' '.$address['postcode']).'">'.$address['address'].'</a>';
-        echo '</p>';
+            // Event and venue meta
+            echo '<p class="entry-meta"><span class="date">'.$datestr.'</span>';
+            if (!empty($venue)) echo ' at <span class="location">'.$venue.'</span>';
+            if (!empty($address['address']) && !empty($address['postcode'])) echo '<br><a class="address" href="http://maps.google.com/maps?q='.urlencode($address['address'].' '.$address['postcode']).'">'.$address['address'].'</a>';
+            echo '</p>';
 
-        // Google calendar link
-        echo '<p><a class="gcal" href="'.eo_get_the_GoogleLink().'">Add to Google Calendar</a></p>';
-        ?>
+            // Google calendar link
+            echo '<p><a class="gcal" href="'.eo_get_the_GoogleLink().'">Add to Google Calendar</a></p>';
+            ?>
+          </div>
+        </div>
       </div>
       <?php get_sidebar(); ?>
     </aside><!-- #complementary -->
